839f51f69a2d151f9cb743c74cfb6560825f0c06
Fabian Roy <fabianroylikhon@gmail.com>
2024-03-13 21:22:02 +0600

    Added blogs.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header';
+import Blogs from './Components/Blogs/Blogs';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       
-      <h1 className='text-6xl '>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
       
     </>
   )
